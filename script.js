18b54c9cd0f4e74670be051457d070d81aac1ea7
ShemeerCoder-01 <118423467+ShemeerCoder-01@users.noreply.github.com>
2023-05-02 00:29:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 //your JS code here. If required.
 let checkbox = document.getElementById("checkbox");
-function check(){
-	if(checkbox.checked){
-		checkbox.checked = false;
-		localStorage.removeItem("username");
-	    localStorage.removeItem("password");
-		
-	}else{
-		checkbox.checked = true;
-	}
-}
+let form = document.getElementById("form");
 
+checkbox.addEventListener('click',()=>{
+		if(!checkbox.checked){
+		// alert(checkbox.checked);
+		localStorage.removeItem("password");
+		localStorage.removeItem("username");
+	}	
+});
 
 
 
@@ -26,14 +24,14 @@ function exisFunc(){
 	alert(`Logged in as ${name}`);
 }
 
-function myFunc(){
+function myFunc(e){
+	e.preventDefault();
 	let username = document.getElementById("username").value;
 	let pass = document.getElementById("password").value;
 	if(checkbox.checked){
-		if(username && pass){
 		localStorage.setItem("username",JSON.stringify(username));
 		localStorage.setItem("password",JSON.stringify(pass));
-		}
+		
 	}else{
 		alert(`Logged in as ${username}`);
 	}
