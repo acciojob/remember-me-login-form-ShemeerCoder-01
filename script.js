1447b8bc6bd619988841b553a2d7031eb6f12c22
ShemeerCoder-01 <118423467+ShemeerCoder-01@users.noreply.github.com>
2023-05-01 23:27:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 //your JS code here. If required.
+let checkbox = document.getElementById("checkbox");
+checkbox.addEventListener('input',()=>{
+	if(checkbox.checked){
+		checkbox.checked = false;
+	}else{
+		checkbox.checked = true;
+	}
+});
+
+if(!checkbox.checked){
+	localStorage.removeItem("username");
+	localStorage.removeItem("password");
+}
 
 document.getElementById("submit").addEventListener('click',myFunc);
 document.getElementById("existing").addEventListener('click',exisFunc);
 if(localStorage.getItem("username") && localStorage.getItem("password")){
-	document.getElementById("form").style.display = "none";
+	// document.getElementById("form").style.display = "none";
 	document.getElementById("existing").style.display = "block";
 }
 
@@ -15,8 +28,7 @@ function exisFunc(){
 function myFunc(){
 	let username = document.getElementById("username").value;
 	let pass = document.getElementById("password").value;
-	let checkbox = document.getElementById("checkbox").value;
-	if(checkbox){
+	if(checkbox.checked){
 		if(username && pass){
 		localStorage.setItem("username",JSON.stringify(username));
 		localStorage.setItem("password",JSON.stringify(pass));
