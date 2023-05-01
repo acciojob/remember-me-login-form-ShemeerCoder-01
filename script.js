b7b89e802b0490cbf1e33450f25cced3b8878f9d
ShemeerCoder-01 <118423467+ShemeerCoder-01@users.noreply.github.com>
2023-05-02 00:37:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,8 +17,6 @@ document.getElementById("existing").addEventListener('click',exisFunc);
 if(localStorage.getItem("username") && localStorage.getItem("password")){
 	// document.getElementById("form").style.display = "none";
 	document.getElementById("existing").style.display = "block";
-}else{
-	document.getElementById("existing").style.display = "none";
 }
 
 function exisFunc(){
