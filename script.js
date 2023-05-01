f8d35948b452fbcb5d3f6308efa2711c86e3d631
ShemeerCoder-01 <118423467+ShemeerCoder-01@users.noreply.github.com>
2023-05-01 22:51:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+
+document.getElementById("submit").addEventListener('click',myFunc);
+document.getElementById("existing").addEventListener('click',exisFunc);
+
+function exisFunc(){
+	let name = JSON.parse(localStorage.getItem("username"));
+	alert(`Logged in as ${name}`);
+}
+
+function myFunc(){
+	let username = document.getElementById("username").value;
+	let pass = document.getElementById("password").value;
+	let checkbox = document.getElementById("checkbox").value;
+	if(checkbox){
+		if(username && pass){
+		localStorage.setItem("username",JSON.stringify(username));
+		localStorage.setItem("password",JSON.stringify(pass));
+		alert(`Logged in as ${username}`);
+		}
+	}
+}
